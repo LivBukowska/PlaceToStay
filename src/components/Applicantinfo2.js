3f6c435a7ba47471cf889358bde1d54bd7ff5923
Jon <haljonathan21@gmail.com>
2022-04-09 20:49:42 -0400

    Added Applicant Info 2

--- src/components/Applicantinfo2.js
+++ src/components/Applicantinfo2.js
@@ -118,7 +118,7 @@ export default function App() {
         </div>
         
         <button class="form-field" type="submit">
-          Continue
+          Submit
         </button>
       </form>
     </div>
