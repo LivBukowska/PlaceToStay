5e79c7c9f9527b69dfc8f0b0a5a5644063410c51
mardenescobar <Escobarmarden510@gmail.com>
2022-04-09 19:43:46 -0700

    Added components for navbar functionality/css

---
+++ src/pages/MatchesPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MatchesPanel() {
+    return (
+        <div className="matchespanel">
+            <p> <span>Logo</span>  Marden Escobar  <span> Other Logo</span></p>
+        </div>
+    )
+}
+
+export default MatchesPanel
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div className="profile">
+            <p> <span>Logo</span>  Marden Escobar  <span> Other Logo</span></p>
+        </div>
+    )
+}
+
+export default Profile
