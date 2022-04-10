7e366df4eafac7e6645df9ef7f3413eea354a9aa
Joanna Folk <joannamfolk@gmail.com>
2022-04-09 22:41:26 -0700

    Got routes up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import "App.css";
-import thumb from "./thumb1.jpg";
+import { render } from "react-dom";
+import {useState} from 'react';
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import Home from "./components/home/Home";
+import Profile from "./components/profile/Profile";
 
-//Import Components
-import Nav from "./Nav";
+const App = () => {
 
-const App = () => (
-  <div>
-    <Nav />
-    <h1 className="wow">Hello React!!!!!</h1>
-    <img src={thumb} alt="" />
-  </div>
-);
+
+  return (
+  
+    <BrowserRouter>
+        <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="profile" element={<Profile />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
